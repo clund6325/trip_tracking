b838f96ded266ef48c3063681725334549f31537
John Doe <paytonanthony2020@gmail.com>
2021-06-05 15:56:36 -0600

    All running good

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 5.times do
   user = User.create(
     email: Faker::Internet.email,
     password: Faker::ChuckNorris.fact
   )
 end
-
 5.times do |i|
-  trip = User.trip.create(
+  trip = Trip.create(
     name: Faker::Address.country,
     user_id: i
   )
 end
-5.times do
+5.times do |i|
   location = Location.create(
-    loc_name: Faker::Nation.capital_city
+    loc_name: Faker::Nation.capital_city,
+    trip_id: i
   )
 end
-
-5.times do
+5.times do |i|
   address = Address.create(
     street: Faker::Address.street_address,
     state: Faker::Address.state,
     city: Faker::Address.city,
     country: Faker::Address.country,
     region: Faker::Address.time_zone,
-    zip: Faker::Address.zip
+    zip: Faker::Address.zip,
+    location_id: i
   )
 end
-
 puts 'Data Seeded'
